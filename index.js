f3e31fdcaf1a8f48d9834822a77f460f6edcbc35
Vanshika <vanshikab06@gmail.com>
2022-02-22 21:26:16 +0530

    JS_part

--- index.js
+++ index.js
@@ -1,81 +1,74 @@
+var buttons = document.querySelectorAll('button');
+var screen = document.querySelector('#input');
+var screenVal = [];
 
-var res=10;
-function calculate(){document.getElementById("input").innerText = res;}
-var screen = '';
-console.log('Working')
-function onec(){
-    screen = screen.concat("1")
-    document.getElementById("input").innerText = screen;
-}
-function twoc(){
-    screen = screen.concat("2")
-    document.getElementById("input").innerText = screen;
-}
-function thrc(){
-    screen = screen.concat("3")
-    document.getElementById("input").innerText = screen;
-}
-function fourc(){
-    screen = screen.concat("4")
-    document.getElementById("input").innerText = screen;
-}
-function fivec(){
-    screen = screen.concat("5")
-    document.getElementById("input").innerText = screen;
-}
-function sixc(){
-    screen = screen.concat("6")
-    document.getElementById("input").innerText = screen;
-}
-function sevenc(){
-    screen = screen.concat("7")
-    document.getElementById("input").innerText = screen;
-}
-function eightc(){
-    screen = screen.concat("8")
-    document.getElementById("input").innerText = screen;
-}
-function ninec(){
-    screen = screen.concat("9")
-    document.getElementById("input").innerText = screen;
-}
-function zeroc(){
-    screen = screen.concat("0")
-    document.getElementById("input").innerText = screen;
-}
-function dotc(){
-    screen = screen.concat(".")
-    document.getElementById("input").innerText = screen;
-}
-function plusc(){
-    screen = screen.concat("+")
-    document.getElementById("input").innerText = screen;
-}
-function minc(){
-    screen = screen.concat("-")
-    document.getElementById("input").innerText = screen;
-}
-function multc(){
-    screen = screen.concat("*")
-    document.getElementById("input").innerText = screen;
-}
-function perc(){
-    screen = screen.concat("%")
-    document.getElementById("input").innerText = screen;
-}
-function dividec(){
-    screen = screen.concat("/")
-    document.getElementById("input").innerText = screen;
-}
-function clearscreen(){
-    screen = ""
-    document.getElementById("input").innerText = screen;
-}
+buttons.forEach((element) => {
 
-function calculateresult(){
-    let result = document.getElementById("input");
-    result.value = eval(screen);
-    console.log(eval(screen))
-    document.getElementById("input").innerText = result.value;
-    screen = "";
-}
+    element.addEventListener("click", () => {
+        if (!element.className.match('remove')) {
+            screenVal.push(element.value);
+            screen.innerText = screenVal.join('');
+            console.log(screenVal);
+        }
+        if (element.id.match('clear')){
+            screenVal = [];
+            screen.innerText = screenVal;
+            console.log(screenVal);
+        }
+        if (element.id.match('backBtn')){
+            screenVal.pop();
+            screen.innerText = screenVal.join('');
+            console.log(screenVal);
+        }
+        if (element.id.match('equal')){
+            let resString = eval(screenVal.join(''));
+            screen.innerText = resString;
+            screenVal = [];
+            // console.log(eval(screenVal.join('')));
+        }
+
+    
+    })   
+});
+    
+
+
+
+
+//
+//     for(let i = 0; i < element.length; i++){
+//     console.log("hey");
+//     }
+// });
+// var arr = ['ji','li','op']
+// arr.forEach(element => {
+//     arr.push(9)
+//     console.log(element+'ji')
+// });
+// console.log(arr)
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function onClickHandler(){
+//     let element = document.getElementsByClassName("column");
+//     for(let i = 0; i < element.length; i++){
+//         element[i].addEventListener("click", function handler(event){
+//             let clickedEvent = event.target.innerText;
+//             document.getElementById("input").innerText = clickedEvent;
+//             console.log("working");
+//         });
+//     }
+// });
+
+// function clearScreen(){
+//     document.getElementById("result").value = "";
+// }
+
+// function calculateresult(){
+//     let result = document.getElementById("input");
+//     result.value = eval(screen);
+//     console.log(eval(screen))
+//     document.getElementById("input").innerText = result.value;
+//     screen = "";
+// }
